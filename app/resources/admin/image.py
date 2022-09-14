7e6b6436a34c4c63425de5597df794cf7e641a6e
dzmitryashkinadze <dima.ashkinadze@gmail.com>
2022-09-14 16:40:44 +0200

    updated the course view

--- app/resources/admin/image.py
+++ app/resources/admin/image.py
@@ -3,7 +3,10 @@ from app.decorators import auth_required
 from werkzeug import datastructures
 from flask import Response, json
 import boto3
+from flask import request
+import base64
 from flask import current_app
+from codecs import encode
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
@@ -17,54 +20,31 @@ def allowed_file(filename):
 # class controlling bundle resource
 class AdminImage(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument(
-        'image', type=datastructures.FileStorage, location='files')
-
-    @auth_required(3)
-    def post(user, self, key):
-        data = AdminImage.parser.parse_args()
-        image = data['image']
-        filename = image.filename
-
-        # Set up the connection to the client
-        s3 = boto3.Session(
-            aws_access_key_id=current_app.config.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=current_app.config.AWS_SECRET_ACCESS_KEY
-        ).resource('s3')
-
-        # check if the file name is valid
-        if allowed_file(filename):
-            # Upload the s3 object
-            try:
-                object = s3.Object('physai-frontend', 'images/' + filename)
-                object.put(Body=image)
-                return {'message': 'image uploaded'}, 200
-            except Exception:
-                return {'message': 'error while uploading'}, 400
-
-        else:
-            return {'message': 'file name is not valid'}, 400
+    parser.add_argument('key')
+    parser.add_argument('base64')
+    parser.add_argument('filename', type=str)
 
     @auth_required(3)
     def delete(user, self, key):
 
         # Set up the connection to the client
         s3 = boto3.Session(
-            aws_access_key_id=current_app.config.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=current_app.config.AWS_SECRET_ACCESS_KEY
+            aws_access_key_id=current_app.config['AWS_ACCESS_KEY_ID'],
+            aws_secret_access_key=current_app.config['AWS_SECRET_ACCESS_KEY']
         ).resource('s3')
 
         # Create the reference to the bucket
-        my_bucket = s3.Bucket('physai-frontend')
+        my_bucket = s3.Bucket('physai-images')
 
         # Find and delete the image
-        for my_bucket_object in my_bucket.objects.filter(Prefix='images/'):
-            if my_bucket_object.key[7:] == key:
+        for my_bucket_object in my_bucket.objects.all():
+            if my_bucket_object.key == key:
                 try:
                     my_bucket_object.delete()
+                    return {'message': 'image deleted'}, 200
                 except Exception:
                     return {'message': 'error while deleting'}, 400
-        return {'message': 'image deleted'}, 200
+        return {'message': 'image not found'}, 400
 
 
 # class controlling bundle resource
@@ -83,9 +63,35 @@ class AdminImageList(Resource):
 
         # Find and return all files in image directory
         images = []
-        for ind, my_bucket_object in enumerate(my_bucket.objects.all()):
-            images.append({"id": ind, "key": my_bucket_object.key})
+        for my_bucket_object in my_bucket.objects.all():
+            images.append({"id": my_bucket_object.key})
 
         response = Response(json.dumps(images))
         response.headers['Content-Range'] = len(images)
         return response
+
+    @auth_required(3)
+    def post(user, self):
+        data = AdminImage.parser.parse_args()
+        b64_string = data['base64'].replace(
+            'data:image/jpeg;base64,', '').strip()
+        img_data = base64.b64decode(b64_string)
+
+        # Set up the connection to the client
+        s3 = boto3.Session(
+            aws_access_key_id=current_app.config['AWS_ACCESS_KEY_ID'],
+            aws_secret_access_key=current_app.config['AWS_SECRET_ACCESS_KEY']
+        ).resource('s3')
+
+        # check if the file name is valid
+        if allowed_file(data.filename):
+            # Upload the s3 object
+            try:
+                object = s3.Object('physai-images', data.filename)
+                object.put(Body=img_data)
+                return {'message': 'image uploaded'}, 200
+            except Exception:
+                return {'message': 'error while uploading'}, 400
+
+        else:
+            return {'message': 'file name is not valid'}, 400
--- app/resources/user/course.py
+++ app/resources/user/course.py
@@ -11,16 +11,26 @@ class Course(Resource):
         chapters = course.chapters
         chapters.sort(key=lambda x: x.sequence_id)
         course_json['chapter_count'] = len(chapters)
+        course_json['test_count'] = len(course.tests)
+        course_json['concept_count'] = len(course.concepts)
+        course_json['problem_count'] = len(course.problems)
+        lesson_count = 0
         chapter_list = []
         for chapter in chapters:
             chapter_json = chapter.json()
             lessons = chapter.lessons
+            lesson_count += len(lessons)
             chapter_json['lesson_count'] = len(lessons)
             lessons.sort(key=lambda x: x.sequence_id)
-            chapter_json['lessons'] = [lesson.json()
-                                       for lesson in lessons]
+            lesson_list = []
+            for lesson in lessons:
+                lesson_json = lesson.json()
+                lesson_json['completed'] = False
+                lesson_list.append(lesson_json)
+            chapter_json['lessons'] = lesson_list
             chapter_list.append(chapter_json)
         course_json['chapters'] = chapter_list
+        course_json['lesson_count'] = lesson_count
         response = Response(json.dumps(course_json))
         response.headers['Content-Range'] = 1
         return response
